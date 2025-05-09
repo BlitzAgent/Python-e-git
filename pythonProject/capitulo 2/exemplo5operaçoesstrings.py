276856c14798d357ca999401df62f43a0dcbfb9a
luis <luis.mendes.almeida@escola.pr.gov.br>
2025-05-09 11:18:54 -0300

    exemplo 5 operaçoes com strangs

---
+++ pythonProject/capitulo 2/exemplo5operaçoesstrings.py
@@ -0,0 +1,10 @@
+# È proibido utilizar operações matematicas com strings
+# Ao menos em  2 casos
+
+first = 'Jose'
+second = 'Valim'
+nomeSobrenome = first + second # + aqui significa concatenção
+print(nomeSobrenome)
+
+StringQualquer = "teste" * 3
+print(StringQualquer)

75a3de540292ad6243c80cf75705ca043a50cae8
luis <luis.mendes.almeida@escola.pr.gov.br>
2025-05-09 12:22:20 -0300

    exercicios 2

--- pythonProject/capitulo 2/exercicios 2.py
+++ pythonProject/capitulo 2/exercicios 2.py
@@ -2,10 +2,10 @@ precoCapa = 24.95
 descontoLivraria = precoCapa * 0.40
 precoCapaLivraria = precoCapa - descontoLivraria
 
-FretePrimeiroExemplar = 3.00
-restanteExemplar = 0.75
+fretePrimeiroExemplar = 3.00
+freteRestanteExemplar = 0.75
 
 custoAtacadoPrimeiroExemplar = precoCapaLivraria + fretePrimeiroExemplar
-custoAtacado= PrimeiroExemplar + ((precoCapaLivraria + freterestanteExemplares) * 59)
+custoAtacado= custoAtacadoPrimeiroExemplar + ((precoCapaLivraria + freteRestanteExemplar) * 59)
 
 print("O custo total de atacado de 60 copias é de; ", custoAtacado)

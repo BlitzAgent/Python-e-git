04d75271ef6938fb6da5e6b5275caffe4d100f18
pepe <pedro.feitosa.andrade@escola.pr.gov.br>
2025-05-16 09:58:10 -0300

    exercicio 3

---
+++ pythonProject/capitulo 2/exercicio 3.py
@@ -0,0 +1,17 @@
+print("tempo do primeiro trecho")
+minutoPrimeiroTrecho = 8
+segundoprimeirotrecho = 15
+totalTempoPrimeiroTrecho = (8 * 60) + 15
+print("tempo do primeiro trecho se segundos: ", totalTempoPrimeirotrecho)
+
+print("tempo do segundo trecho: 7min e 12seg")
+minutosSegundoTrecho = 7
+segundosSegundoTrecho = 12
+totalTempoSegundoTrecho = (7 * 60) + 15
+print("tempo de segundo trecho em segundos: ", totalTempoSegundoTrecho)
+
+print("Tempo do terceiro trecho: 8 min e 15 s")
+minutoTerceiroTrecho = 8
+segundoTerceiroTrecho = 15
+totalTempoTerceiroTrecho = (8 * 60) + 15
+print("Tempo do terceiro trecho em segundo ", totalTempoTerceiroTrecho)
